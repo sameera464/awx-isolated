94257b90c89fa43ebfe2476b3036a0b8ebbe182c
Joel Guilarte <Jguilarte@rccl.com>
2018-10-19 15:45:13 -0400

    updating to awx 2.0.1

--- docker/awx/awx/ui/test/unit/components/index.js
+++
@@ -1,9 +0,0 @@
-// Import angular and angular-mocks to the global scope
-import 'angular-mocks';
-
-// Import tests
-import './file.unit';
-import './layout.unit';
-import './side-nav.unit';
-import './side-nav-item.unit';
-
--- docker/settings/launch_isolated.sh
+++
@@ -1,4 +0,0 @@
-awx-manage register_instance --hostname=isolated
-awx-manage register_queue --queuename='thepentagon' --hostnames=isolated --controller=tower
-
-/usr/bin/launch_awx_task.sh
