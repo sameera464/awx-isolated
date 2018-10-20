08dfcb66d2ef1ff6788dfbd1b13f1ea64ac79e5e
Joel Guilarte <Jguilarte@rccl.com>
2018-10-19 21:19:27 -0400

    fixed isolated node issues, related to docker container file structure and ssh connectivity between task and isolated node

--- docker/build.sh
+++ docker/build.sh
@@ -6,12 +6,14 @@ mkdir -p ./data/postgres
 DOCKER_IMAGE=devops/awx
 DOCKER_TAG=develop
 
+AWX_VERSION=2.0.1
+
 # Remove previous image and recreate new image
 docker rmi -f $DOCKER_IMAGE-isolated:$DOCKER_TAG
-docker build -t $DOCKER_IMAGE-isolated:$DOCKER_TAG -f Dockerfile.isolated .
+docker build -t $DOCKER_IMAGE-isolated:$DOCKER_TAG -f Dockerfile.isolated --build-arg  AWX_VERSION=$AWX_VERSION .
 
 docker rmi -f $DOCKER_IMAGE-task:$DOCKER_TAG
-docker build -t $DOCKER_IMAGE-task:$DOCKER_TAG -f Dockerfile.task .
+docker build -t $DOCKER_IMAGE-task:$DOCKER_TAG -f Dockerfile.task --build-arg  AWX_VERSION=$AWX_VERSION .
 
 docker rmi -f $DOCKER_IMAGE-web:$DOCKER_TAG
-docker build -t $DOCKER_IMAGE-web:$DOCKER_TAG -f Dockerfile.web .
+docker build -t $DOCKER_IMAGE-web:$DOCKER_TAG -f Dockerfile.web --build-arg  AWX_VERSION=$AWX_VERSION .
